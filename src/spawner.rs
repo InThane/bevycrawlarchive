74850e7f5edcbada39687e215ec5d427ef1ffe98
InThane <inthane@gmail.com>
2021-09-11 13:51:50 -0700

    Collision system added

--- src/spawner.rs
+++ src/spawner.rs
@@ -13,3 +13,25 @@ pub fn spawn_player(ecs : &mut World, pos : Point) {
         )
     );
 }
+
+/// Spawns a random monster at the given point.
+pub fn spawn_monster(
+    ecs: &mut World,
+    rng: &mut RandomNumberGenerator,
+    pos: Point
+) {
+    ecs.push(
+        (Enemy,
+            pos,
+            Render{
+                color: ColorPair::new(DF_FG_COLOR, DF_BG_COLOR),
+                glyph: match rng.range(0,4) {
+                    0=> to_cp437('E'),
+                    1=> to_cp437('O'),
+                    2=> to_cp437('o'),
+                    _=> to_cp437('g'),
+                }
+            }
+        )
+    );
+}
